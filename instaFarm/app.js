7e1ce755b04f495384712898a4ed9936989565f6
Samuel <atulesamuel20@gmail.com>
2019-04-27 17:35:24 +0000

    .

--- instaFarm/app.js
+++ instaFarm/app.js
@@ -12,9 +12,9 @@ var app = express();
 var debug = require('debug')('lpg:app.js');
 mongoose.Promise = require('bluebird');
 mongoose.connect(database.url, {
-  reconnectTries: Number.MAX_VALUE,
-  reconnectInterval: 1000,
-  useNewUrlParser: true
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    useNewUrlParser: true
 });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,36 +31,36 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
 
 // MONGOOSE DEFAULTS
 mongoose.connection.on('connected', function() {
-  debug('Mongoose default connection connected');
+    debug('Mongoose default connection connected');
 });
 mongoose.connection.on('error', function(err) {
-  debug('Mongoose default connection error:' + err);
+    debug('Mongoose default connection error:' + err);
 });
 mongoose.connection.on('disconnected', function() {
-  debug('Mongoose default connection disconnected');
+    debug('Mongoose default connection disconnected');
 });
 process.on('SIGINT', function() {
-  mongoose.connection.close(function() {
-    console.log('Mongoose default connection disconnected on app termination');
-    debug('Mongoose default connection disconnected on app termination');
-    process.exit(0);
-  });
+    mongoose.connection.close(function() {
+        console.log('Mongoose default connection disconnected on app termination');
+        debug('Mongoose default connection disconnected on app termination');
+        process.exit(0);
+    });
 });
